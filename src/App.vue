1459791534d41d9e80c44c81b11aad7d11835df1
shafou <jl_macedomatos@hotmail.com>
2020-09-03 17:14:25 -0400

    Added pretty column and testing fonts"

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,54 @@
 <template>
-	<div class="container-fluid"> 
-		<div class="row">
-			<div class="col-md-3 position-fixed">
-				<vertical-sidebar></vertical-sidebar>
-			</div>
+  <div class="container-fluid"> 
+    <div class="row">
+      <div class="col-md-3 position-fixed">
+        <div class="row">
+          <div class="col-md-11">
+            <vertical-sidebar></vertical-sidebar>
+          </div>
+          <div class="col-md-1">
+          </div>
+        </div>
+      </div>
       <div class="col-md-3">
       </div>
-			<div class="col-md-9">
-				<main-content></main-content>
-			</div>
-		</div>
-	</div>
+      <div class="col-md-9">
+        <main-content></main-content>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import MainContent from './components/MainContent.vue'
 import VerticalSidebar from './components/VerticalSidebar.vue'
 export default {
-	components: { VerticalSidebar, MainContent }
+  components: { VerticalSidebar, MainContent }
 }
 </script>
 
 <style>
+@font-face { font-family: Connection; src: url('./assets/ConnectionIi-2wj8.otf'); }
+
+body { 
+  font-family: Connection, sans-serif !important;
+}
 .container-fluid {
-	padding: 0 !important;
-	height: 100vh;
+  padding: 0 !important;
+  height: 100vh;
 }
 .row {
   margin: 0 !important;
 }
 .position-fixed {
   padding: 0px !important;
+}
+.col-md-11, .col-md-1 {
+  padding: 0px !important;
+}
+.col-md-1 {
+  background-image: url("./assets/block2.png");
+  background-repeat: repeat-y;
 }
 .col-md-9 {
   margin-left: 25vw !important;
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -37,6 +37,7 @@ export default {
 </script>
 
 <style scoped>
+.col-md-1 { background: none }
 .img-container { height: 25rem ; width: 50rem ; margin: auto; padding: 15px; }
 img { height: 100%; width: 100%; }
 .row { margin: 15px !important; padding: 15px !important; }
